6805b735f1d370fe8f0f1fe887d0775b393117d2
xinxi.li <xinxi.li@dianping.com>
2020-08-27 22:34:17 +0800

    sqrt2

---
+++ src/main/java/com/kunbetter/future/Sqrt2.java
@@ -0,0 +1,19 @@
+package com.kunbetter.future;
+
+/**
+ * User: xinxi.li
+ * Date: 2020/8/27
+ * Time: 22:31
+ */
+public class Sqrt2 {
+
+    // https://blog.csdn.net/xiaxl/article/details/94438829
+
+    public static void main(String[] argv) {
+
+    }
+
+    public static double sqrt2(int m) {
+        return 0D;
+    }
+}
